00edd62cd41ea1cb52d19e3e4ffa2c82419e70fb
Manamiz <thibduch@hotmail.com>
2015-09-25 15:06:06 +0200

    sijjsa

--- server.js
+++ server.js
@@ -1,8 +1,34 @@
-var http = require('http');
+var http = require("http");
+/**
 
-var server = http.createServer(function(request, response) {
-	response.writeHead(200);
-	response.end('Thibaud & Mathias & Valentin ... Hello');
+* This function starts a http daemon on port 9000. It also
+
+* registers a callback handler, to handle incoming HTTP
+* requests (a simple message is sent back to clients).
+*/
+function runHttpServer() {
+var daemon = http.createServer();
+
+daemon.on("request", function (req, res) {
+console.log("A request has arrived: URL=" + req.url);
+var myObject = {
+firstName: "Olivier",
+lastName: "Liechti",
+address: {
+street: "Grand-Rue 14",
+zip: "1446"
+}
+};
+
+res.writeHead(200, {
+'Content-Type': 'application/json'
+});
+res.end(JSON.stringify(myObject));
 });
 
-server.listen(8080);
+console.log("Starting http daemon...");
+var port = process.env.PORT || 3000;
+daemon.listen(port);
+}
+
+runHttpServer();
