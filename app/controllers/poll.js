36d228324eb650a6a7e891fd3647060efa17ef23
Manamiz <thibduch@hotmail.com>
2015-10-29 14:22:45 +0100

    Projet

---
+++ app/controllers/poll.js
@@ -0,0 +1,74 @@
+var express = require('express'),
+   router = express.Router(),
+   mongoose = require('mongoose'),
+   Poll = mongoose.model('Poll');
+
+module.exports = function(app) {
+   app.use('/', router);
+};
+
+// Affiche les polls
+router.get('/polls', function(req, res) {
+   Poll.find(function(err, polls) {
+      if (err) throw err;
+
+      res.render('polls', {
+         polls: polls
+      });
+   });
+});
+
+// Ajoute un nouveau poll
+router.post('/polls', function(req, res) {
+   var newPoll = new Poll({
+      title: req.body.title,
+      state: req.body.state
+   });
+   newPoll.save(function(err) {
+      if (err) throw err;
+   });
+
+   res.redirect('back')
+});
+
+// Delete un poll
+router.delete('/polls/:id', function(req, res) {
+   Poll.remove({
+      _id: req.params.id
+   }, function(err) {
+      if (err) throw err;
+
+      res.send('DELETE success');
+   });
+});
+
+router.put('/polls/:id', function(req, res) {
+   Poll.findByIdAndUpdate(req.params.id, {
+      title: req.body.title,
+      state: req.body.state
+   }, function(err, model) {
+      if (err) throw err;
+
+      res.send('PUT success');
+   });
+});
+
+// Affiche les détails d'un poll et permet de le modifier
+router.get('/polls/:id', function(req, res) {
+   Poll.findById(req.params.id, function(err, poll) {
+      res.render('edit-poll', {
+         poll: poll
+      });
+   });
+});
+
+
+router.post('/polls/edit', function(req, res) {
+   Poll.findByIdAndUpdate(req.body.id, {
+      title: req.body.title,
+      state: req.body.state
+   }, function(err, model) {
+      if (err) throw err;
+   });
+   res.redirect('/polls');
+});
---
+++ app/models/poll.js
@@ -0,0 +1,58 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var PollSchema = new Schema({
+   title: String,
+   creationDate: {
+      type: Date,
+      default: Date.now
+   },
+   state: String
+});
+
+var PollModel = mongoose.model('Poll', PollSchema);
+
+/* POUR LES TESTS */
+
+PollModel.remove({}, function(err) {
+   if (err) throw err;
+
+   console.log('Table "Poll" vidée avec succés');
+});
+
+var poll = new PollModel({
+   title: 'Poll 1',
+   state: 'active'
+});
+poll.save(null);
+
+poll = new PollModel({
+   title: 'Poll 2',
+   state: 'active'
+});
+poll.save(null);
+
+poll = new PollModel({
+   title: 'Poll 3',
+   state: 'active'
+});
+poll.save(null);
+
+poll = new PollModel({
+   title: 'Poll 4',
+   state: 'closed'
+});
+poll.save(null);
+
+poll = new PollModel({
+   title: 'Poll 5',
+   state: 'closed',
+   creationDate: '2011-04-11'
+});
+poll.save(null);
+
+poll = new PollModel({
+   title: 'Poll 6',
+   state: 'drafti'
+});
+poll.save(null);
